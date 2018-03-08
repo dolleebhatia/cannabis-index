4779991a887ad13cc26c6baf9d7aa1eaf73149dc
dollee <dolleebhatia@users.noreply.github.com>
2018-03-08 11:53:51 -0500

    with a diff thetha and phi

--- sketch.js
+++ sketch.js
@@ -6,7 +6,6 @@ let img;
 let rx = 0;
 let ry = 0;
 let earthradius =400;
-let boxheight = 100;
 let angle = 0;
 
 particles = [];
@@ -21,7 +20,7 @@ let zoomZ = 0;
 
 function preload() {
 
-  img = loadImage('earth.jpg');
+  img = loadImage('world.jpg');
 //  mapimg = loadImage('https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/-77.03968,-0.00000,0.2,0,0/1024x1024@2x?access_token=pk.eyJ1IjoiZG9sbGVlIiwiYSI6ImNqZTh5dWFjZzA0dTQyd25zdnJ1YjFwc2YifQ.uVTRMcrpvTQo1m13yZf--Q');
 //  mapimg = loadImage('https://api.mapbox.com/styles/v1/mapbox/cj5banmps1bqr2rqwsy5aeijw/static/' +
   //    clon + ',' + clat + ',' + zoom + '/' +
@@ -53,7 +52,7 @@ function setup(){
   canjsoncities = canjson.cities;
   let r = earthradius;
 
-  for (let i = 0; i < canjsoncities.length; i++) {
+  for (let i = 0; i < canjsoncities.length ; i++) {
       //let data = candata[i].split(/,/);
     let city = canjsoncities[i].city;
     let lon = (canjsoncities[i].longitude);
@@ -61,22 +60,19 @@ function setup(){
     let mag = canjsoncities[i].consumption;
          console.log(city+ " lat:"+lat+" lon:"+lon+" mag:"+mag);
 
-    // float cx = alt * cos(lat/theta) * cos(lon);
-    // float cy = alt * cos(lat/theta) * sin(lon);
-    // float cz = alt * sin(lat/theta);
+    // float cx = alt * cos(lat/phi) * cos(lon);
+    // float cy = alt * cos(lat/phi) * sin(lon);
+    // float cz = alt * sin(lat/phi);
     // so x = -cx, y = -cz, z = cy
 //float x = -cx, y = -cz, z = cy;
 
 ////function calcPosFromLatLonRad(lat,lon,radius){
-var phi   = (90-lat)*(Math.PI/180);
-var theta = (90-lon)*(Math.PI/180);
-// let theta = radians(lat);
-// let phi = radians(lon);
+let thetha = (90-lat)*(Math.PI/180);
+let phi = (90-lon)*(Math.PI/180);
 
-
-// x = -((radius) * Math.sin(phi)*Math.cos(theta));
-// z = ((radius) * Math.sin(phi)*Math.sin(theta));
-// y = ((radius) * Math.cos(phi));
+// x = -((radius) * Math.sin(thetha)*Math.cos(phi));
+// z = ((radius) * Math.sin(thetha)*Math.sin(phi));
+// y = ((radius) * Math.cos(thetha));
 // 	console.log([x,y,z]);
 //    return [x,y,z];
 // }
@@ -86,26 +82,22 @@ var theta = (90-lon)*(Math.PI/180);
 // y =  R * sin(LAT)
 // z =  R * cos(LAT) * sin(LON)
 
-// var phi = (90 - lat) * Math.PI / 180;
-//   var theta = (180 - lng) * Math.PI / 180;
+// var thetha = (90 - lat) * Math.PI / 180;
+//   var phi = (180 - lng) * Math.PI / 180;
 //
-//   point.position.x = 200 * Math.sin(phi) * Math.cos(theta);
-//   point.position.y = 200 * Math.cos(phi);
-//   point.position.z = 200 * Math.sin(phi) * Math.sin(theta);
+//   point.position.x = 200 * Math.sin(thetha) * Math.cos(phi);
+//   point.position.y = 200 * Math.cos(thetha);
+//   point.position.z = 200 * Math.sin(thetha) * Math.sin(phi);
 
-
-    let cX = -(r * sin(phi) * cos(theta));
-    let cZ = -(r * sin(theta)* sin(phi));
-    let cY = -(r * cos(phi));
+    let cX = -(r * sin(thetha) * cos(phi));
+    let cZ = -(r * sin(phi)* sin(thetha));
+    let cY = -(r * cos(thetha));
     let posvector = createVector(cX, cY, cZ);
     let xaxis = createVector(1, 0, 0);
     let raxis = p5.Vector.cross(xaxis,posvector);
     angleMode(RADIANS);
     let angleb = p5.Vector.angleBetween(xaxis,posvector);
     //let angleb = atan2(posvector, dx);
-
-
-
 
     mag = pow(mag,2);
     mag = sqrt(mag);
@@ -129,11 +121,6 @@ var theta = (90-lon)*(Math.PI/180);
     label[i].textAlign(CENTER);
     label[i].textSize(12);
 
-      //stroke(255, 0, 255,90);
-      //fill(255, 0, 255, 200);
-      //ellipse(x,y, 4, 4, 100);
-      //console.log(x,y,d);
-        //noStroke();
   }
 
 
@@ -170,9 +157,9 @@ function draw(){
 //  ambientLight(100,200,255);
 
   let cityname;let x_axis;let r_axis;let d_mag;let angle_b;
-  let x; let y; let z;
+  let x; let y; let z; let boxheight;
     //for (let i = canjsoncities.length - 1; i >= canjsoncities.length - 5; i--) {
-      for (let i = 0; i< canjsoncities.length; i++){
+      for (let i = 0; i< canjsoncities.length ; i++){
       d_mag = cityxy.get(i,1);
       cityname = cityxy.get(i,2);
       cX = cityxy.get(i,3);
@@ -183,7 +170,7 @@ function draw(){
 
       // so x = -cx, y = -cz, z = cy
   //float x = -cx, y = -cz, z = cy;
-
+      boxheight = d_mag*20 - earthradius/2;
       x = cX;
       y = cY;
       z = cZ;
@@ -191,24 +178,25 @@ function draw(){
       //add z
       //make new vector xaxis and angle between
       if (!consolecont){
-        console.log(cityname + " x:"+ x+ " y:" + y+ " z:"+z +" angle_b:"+angle_b+" r_axis.z:"+r_axis.z);
+        // + " x:"+ x+ " y:" + y+ " z:"+z +
+        console.log(cityname+ " angle_b:"+angle_b+" r_axis.z:"+r_axis.z);
       }
 
       //for (let j=0; j < round(1+dmag); j++) {
         push();
           translate(x,y,z);
-          rectMode(CORNER);
+          rectMode(CENTER);
           angleMode(DEGREES);
           //rotate(angle_b, [r_axis.x, r_axis.y, r_axis.z]);
-             rotateZ(angle_b, r_axis.z);
-             rotateY(angle_b, r_axis.y);
-             rotateX(angle_b, r_axis.x);
+              //rotateZ(angle_b, abs(r_axis.z));
+            //  rotateY(angle_b, abs(r_axis.y));
+            //  rotateX(angle_b, abs(r_axis.x));
 
           fill(255);
           //label[i].text(label[i], 50, 50);
           //texture(label[i]);
           normalMaterial();
-            box(d_mag*10,3,3);
+            box(2,40,2);
         pop();
 
   }
