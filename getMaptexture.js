80635772cc65d81b5f6f271a22dc749d95bc7d6c
dollee <dolleebhatia@users.noreply.github.com>
2018-03-11 14:20:43 -0400

    -working

--- getMaptexture.js
+++ getMaptexture.js
@@ -1,6 +1,9 @@
 //Add callback
 
-function getMaptexture( ){
+//D3
+function getMaptexture(geojson,color){
+
+    var texture, context, canvas;
 
   var projection = d3.geo.equirectangular()
     .translate([512, 256]).scale(163);
@@ -16,17 +19,22 @@ function getMaptexture( ){
       .attr({width: "1024px", height: "512px"})
       .style("display", "none");
 
-    var context = canvas.node().getContext("2d");
+    context = canvas.node().getContext("2d");
     var path = d3.geo.path()
       .projection(projection).context(context);
 
     context.strokeStyle = "#333";
     context.lineWidth = 0.25;
-    context.fillStyle = "#222";
+    context.fillStyle = color || "#222";
     context.beginPath();
     path(countries);
+    //path(geojson);
     context.fill();
     context.stroke();
+      if (color) {
+        context.fill();
+        }
+        context.stroke();
     maptextureurl = loadImage(canvas.node().toDataURL('image/png'),function() {console.log('loaded');d3loaded = true;});
     return maptextureurl;
     canvas.remove();
--- sketch.js
+++ sketch.js
@@ -1,36 +1,31 @@
 
 let ww = 1024;
-let hh = 512;
+let hh = 768;
 let mapimg;
 let img;
 let maptextureurl;
 let textureW = 1024;
-let textureH=512;
+let textureH=600;
 let path;
 let d3loaded = false;
-let rx = 0;let ry = 0;let earthradius =200;let angle = 0;
+let rx = 0;let ry = 0;let r =300;let angle = 0;
+let city3d;
 
 rows = [];label = [];
-let cityxy;let candata;let canjsoncities;let r = earthradius;let zoomZ = -50;
+let cityxy;let candata;let canjsoncities;let zoomZ = 0;
 
 function preload() {
-
+  city3d = loadModel('NYC.obj');
   getMaptexture();
   console.log(maptextureurl);
   //add callback
 
   img = loadImage('world.jpg');
-//  mapimg = loadImage('https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/-0.000,0.00000,0.2,0,0/1280x1280@2x?access_token=pk.eyJ1IjoiZG9sbGVlIiwiYSI6ImNqZTh5dWFjZzA0dTQyd25zdnJ1YjFwc2YifQ.uVTRMcrpvTQo1m13yZf--Q');
-  //mapimg = loadImage('https://api.mapbox.com/styles/v1/mapbox/cj5banmps1bqr2rqwsy5aeijw/static/' +
-      //clon + ',' + clat + ',' + zoom + '/' +
-//  ww + 'x' + hh +'?access_token=pk.eyJ1IjoiZG9sbGVlIiwiYSI6ImNqZTh6MmgyeDAwYTAzM3M1dmdnN3BhMW8ifQ.LEZ3tpEA4sLQ-UfanFHnmQ');
 
   // Load Cannabis Data. Works with both CSV and JSON files
  // //candata = loadStrings('candata.csv');
     canjson = loadJSON("candata.json");
 }
-
-
 
 function setup(){
   createCanvas(ww,hh, WEBGL);
@@ -45,6 +40,10 @@ function setup(){
   cityxy.addColumn('xaxis');
   cityxy.addColumn('raxis');
   cityxy.addColumn('angleb');
+  cityxy.addColumn('country');
+  cityxy.addColumn('legality');
+
+
   canjsoncities = canjson.cities;
   for (let i = 0; i < canjsoncities.length; i++) {
       //let data = candata[i].split(/,/);
@@ -52,6 +51,8 @@ function setup(){
     let lon = (canjsoncities[i].longitude);
     let lat = (canjsoncities[i].latitude);
     let mag = canjsoncities[i].consumption;
+    let country = canjsoncities[i].country;
+    let legality = canjsoncities[i].legality;
 
 let thetha = PI/2 +radians(lat);
 let phi = PI/2 - radians(lon) ;
@@ -82,6 +83,9 @@ let phi = PI/2 - radians(lon) ;
     rows[i].set('xaxis', xaxis);
     rows[i].set('raxis', raxis);
     rows[i].set('angleb', angleb);
+    rows[i].setString('country', country);
+    rows[i].setString('legality', legality);
+
     label[i] = createGraphics(100, d);
     label[i].fill(255);
     label[i].textAlign(CENTER);
@@ -94,17 +98,19 @@ zoomZ += event.delta;
 //uncomment to block page scrolling
 //return false;
 }
-let consolecont = false;
+let beginRotate = false;
+
 function draw(){
 
-  background(50);
-  translate(0, 0, zoomZ);
+  background(20);
+  translate(0, 40, zoomZ);
   rotateY(rx);
   rotateX(ry);
   if (d3loaded){texture(maptextureurl);}
   else {setTimeout(function(){ console.log("timeout3000"); }, 3000);}
   texture(maptextureurl);
-    sphere(earthradius);
+
+    sphere(r);
 
   // Rotate the globe if the mouse is pressed
   if (mouseIsPressed) {
@@ -115,7 +121,8 @@ function draw(){
   let dirY = mouseY - height / 2;
 
   //directionalLight(10,10,10, width/2, height/2, 0);
-  directionalLight(255,255,255, 0, -1, -1);
+  pointLight(255,255,255, 0, -1, -1);
+  pointLight(255,255,255, -1, -1, -1);
   ambientLight(255,255,255);
 
 
@@ -130,15 +137,15 @@ function draw(){
       r_axis = cityxy.get(i,7);
       angle_b = (cityxy.get(i,8));
 
-      boxheight = d_mag*10 - earthradius/2;
+      boxheight = d_mag*10 - r/2;
       x = cX;
       y = cY;
       z = cZ;
 
-      if (!consolecont){
+      //if (!consolecont){
         // + " x:"+ x+ " y:" + y+ " z:"+z +
         //console.log(cityname+ " angle_b:"+angle_b+" r_axis.z:"+r_axis.z);
-      }
+      //}
       push();
         translate(x,y,z);
         rotate(angle_b, [r_axis.x, r_axis.y, -r_axis.z]);
@@ -149,10 +156,34 @@ function draw(){
       fill(255);
         //label[i].text(label[i], 50, 50);
         //texture(label[i]);
+
+      //if (cityname == "New York"){
+        //push();
+        //scale(0.09);
+        //translate(200,0,0);
+        //rotateZ(90);
+        //box(box,3,3);
+        //model(city3d);
+        //pop();
+      //} else {
       normalMaterial();
       box(boxheight,3,3);
+  //  }
       pop();
 
-  }
-  consolecont = true;
+
+    }
+//  beginRotate = true;
+}
+
+function mousePressed() {
+  let latRads = -acos(mouseX*r);
+  let lonRads = -atan2(r*mouseY);
+  let lat = PI/2 - degrees(latRads);
+  let lon = PI - degrees(lonRads);
+  console.log(latRads, lonRads);
+  console.log(lat, lon-180);
+
+  // let thetha = PI/2 +radians(lat);
+  // let phi = PI/2 - radians(lon) ;
 }
